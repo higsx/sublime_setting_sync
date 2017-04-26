04a469ee07b260174e3549cc1c005eef5080af58
gsx <higsx@qq.com>
2017-04-26 16:07:17 +0800

    update sublime

---
+++ sublime_setting/mklink_cmd_for_debug.bat
@@ -0,0 +1,5 @@
+mklink "C:\Users\Administrator\AppData\Roaming\Sublime Text 3\Packages\User\close_other.py" "E:\work\git\sublime_setting_sync\sublime_setting\close_other.py"
+mklink "C:\Users\Administrator\AppData\Roaming\Sublime Text 3\Packages\User\Default (Windows).sublime-keymap" "E:\work\git\sublime_setting_sync\sublime_setting\Default (Windows).sublime-keymap"
+mklink "C:\Users\Administrator\AppData\Roaming\Sublime Text 3\Packages\User\mklink_cmd_for_debug.bat" "E:\work\git\sublime_setting_sync\sublime_setting\mklink_cmd_for_debug.bat"
+mklink "C:\Users\Administrator\AppData\Roaming\Sublime Text 3\Packages\User\Package Control.sublime-settings" "E:\work\git\sublime_setting_sync\sublime_setting\Package Control.sublime-settings"
+mklink "C:\Users\Administrator\AppData\Roaming\Sublime Text 3\Packages\User\Preferences.sublime-settings" "E:\work\git\sublime_setting_sync\sublime_setting\Preferences.sublime-settings"
